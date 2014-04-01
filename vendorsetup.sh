121fdcbf088790ffd9765b681505287ce75193aa
jb2kred <jb2kred@gmail.com>
2014-04-01 17:54:03 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -32,8 +32,8 @@ add_lunch_combo pa_n7100-userdebug
 add_lunch_combo pa_odin-userdebug
 add_lunch_combo pa_p880-userdebug
 add_lunch_combo pa_skyrocket-userdebug
+add_lunch_combo pa_tate-userdebug
 add_lunch_combo pa_togari-userdebug
 add_lunch_combo pa_v500-userdebug
 add_lunch_combo pa_vs980-userdebug
 add_lunch_combo pa_yuga-userdebug
-add_lunch_combo pa_tate-userdebug
