b1f58dba5ca6aed7369237d81a4f27e3e7f1ba62
darknightmare <rubentkd98@gmail.com>
2014-10-16 22:29:39 +0200

    Add Xiaomi Redmi 1S (armani) support
    Change-Id: I215a000a9f2b85d9148036f18846d8e4ae891d15

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -18,6 +18,9 @@ endif
 ifeq (pa_aries,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_aries.mk
 endif
+ifeq (pa_armani,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_armani.mk
+endif
 ifeq (pa_d2lte,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_d2lte.mk
 endif
